09424373466553b91574a7ac8f5ab4e38cf19694
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-09-05 17:26:33 -0400

    Deleted and polished development comments and further edited the readme

--- script.js
+++ script.js
@@ -34,69 +34,11 @@ function generate_password()
  }
   for (let i = 1; i <= quantity; i++) { // loop to create password
     let type_selection = Math.floor(Math.random() * type.length); // gives me a random number between 0-4 (the length of "type")
-    let character_selection = Math.floor(Math.random() * type[type_selection].length); //gives me a random index in the string
-    password = password + type[type_selection][character_selection]; //generating the whole password
+    let character_selection = Math.floor(Math.random() * type[type_selection].length); //gives me a random index from randomly selected string (which loops depending on quantity)
+    password = password + type[type_selection][character_selection]; //generates the password output
   }
-  //checks if the password is working as intended
-  //now that i have the password solved, this finds the text area, and gives the "value" which is the text box content
    document.querySelector("#textarea").value = password //finding text area's "value (or) content" then giving it the value of password
 }
 let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", generate_password);
-
-
-// document.getElementById()
-//document.querySelector('textarea') = function seems like the right thing, but where?**
-
-
-
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", generate_password);
-
-// int index=Math.floor
-// for (let i = 1; i )
-// lowercase
-// Math.floor(Math.random() * 10)
-
-// Assignment Code
-
-// var generateBtn = document.querySelector("#generate");
-// console.log(generateBtn)
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword("");
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// writePassword()
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// // ACII table conversions
-// // arrays
-// lower_case
-// // hex value 61-7a
-// // 97-122 decimal value
-// upper_case
-// // decimal value
-// // 65-90
-// numbers
-// //decimal value
-// // 0-9
-// special
-// // ! @ # $ % ^ & ( )
-
-// // var password_potential = {
-// //     number: number,
-// //     alphabetLOW: alphabetLOW,
-// //     alphabetCAP: alphabetCAP
-// //     specialCHAR: specialCHAR
-// // } ;
-
-// // var pword-loop = '';
-// // for (var i  in pword-potential)
+// this was copied directly from the starter code.
